476c57e051080a1d2187b9e756f163c84b8e2beb
irinebabu00 <irinebabu2000@gmail.com>
2025-02-28 18:26:08 +0100

    Create graphs

--- graph_search_task.py
+++ graph_search_task.py
@@ -22,6 +22,17 @@ with open("feature_graph.json", "r") as fgfile:
 # Note: Other appraoches are also possible.
 
 # TODO
+def construct_graph(graph_data):
+    G = nx.Graph()
+    for node, attrs in graph_data["nodes"]:
+        node_id, node_attrs = node, attrs
+        G.add_node(node_id, **node_attrs)
+    for edge in graph_data["edges"]:
+        node1, node2, edge_attrs = edge
+        G.add_edge(node1, node2, **edge_attrs)
+    return G
+workpiece_graph_new = construct_graph(workpiece_graph)
+feature_graph_new = construct_graph(feature_graph)
 
 # Note: Optional task - Visualize the graph
 # Example code:
